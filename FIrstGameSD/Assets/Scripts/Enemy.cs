98acacf74f0ee30e397b531bd1a0640bb19a890f
drakearpetor <bengal.ty41@gmail.com>
2019-06-14 16:03:06 -0400

    PlayerDamage, EnamyHealth&Damage
    i've made the Enamy Health and Damage scripts as whell as the Player Damage script

--- FIrstGameSD/Assets/Scripts/Enemy.cs
+++ FIrstGameSD/Assets/Scripts/Enemy.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] float health = 100;
+    [SerializeField] float shothCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemyProgetile;
+    [SerializeField] float projectileSpeed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shothCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shothCounter -= Time.deltaTime;
+        if (shothCounter <= 0f)
+        {
+            Fire();
+            shothCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject progetile = Instantiate(
+           enemyProgetile,
+           transform.position,
+           Quaternion.identity
+           ) as GameObject;
+        progetile.GetComponent<Rigidbody2D>().velocity = new Vector2(-projectileSpeed, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProsesHit(damageDealer);
+    }
+
+    private void ProsesHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
