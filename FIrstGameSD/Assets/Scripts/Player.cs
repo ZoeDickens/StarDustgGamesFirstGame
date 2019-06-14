466be4973ebfbfb36fa7676234e6f86d8615ec25
drakearpetor <bengal.ty41@gmail.com>
2019-06-14 13:54:50 -0400

    Test Sprites & Projectiles
    i made some Test Sprites & Projectiles that will be used to set up the health and damage scripts.

---
+++ FIrstGameSD/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] GameObject PlayerProgetilePF;
+    [SerializeField] float projectileSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject progetile = Instantiate(PlayerProgetilePF, transform.position, Quaternion.identity) as GameObject;
+            progetile.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileSpeed, 0);
+        }
+    }
+
+}
